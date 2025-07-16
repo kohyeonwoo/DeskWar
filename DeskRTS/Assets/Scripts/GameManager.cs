99f3900472bca93462a65c910fa4b152c1b0d93e
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2025-07-16 21:23:56 +0900

    250716
    바닥 추가 및 머테리얼 적용, AI Navigation Unity Registry 추가 및 반영

--- DeskRTS/Assets/Scripts/GameManager.cs
+++ DeskRTS/Assets/Scripts/GameManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static GameManager Instance;
+
+    public List<Unit> allUnit;
+
+    private void Start()
     {
-        
+        if(Instance == null)
+        {
+            Instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RegistUnit(Unit units)
     {
-        
+           allUnit.Add(units);
     }
 }
